cce3969ffffbf2d44ada5e6b75319d06541aaa67
Gordon <54365930+gorgorgordon@users.noreply.github.com>
2020-03-22 12:37:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
-
 const app = express();
-
 app.use(express.static("public"));
 
+const path = require('path');
+
+
 app.get('/', (req, res) => {
-  res.render('./index.html');
+  var options = {
+    root: path.join(__dirname, 'public'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  res.sendFile('index.html', options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
 })
 
 app.listen(process.env.PORT || 3000, function() {
