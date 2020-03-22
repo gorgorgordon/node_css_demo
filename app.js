66d299b12fad1903298f82774e9ba1db4d36a790
Gordon <54365930+gorgorgordon@users.noreply.github.com>
2020-03-22 12:43:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
-const path = require('path');
-
-
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
